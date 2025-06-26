ac2b9ccce7ff6b9d0d5f505522555889724f4529
poper12 <samyakdhengle89@gmail.com>
2025-06-27 00:49:13 +0530

    config.py

--- config.py
+++ config.py
@@ -5,21 +5,21 @@ load_dotenv()
 
 env_vars = {
   # Get From my.telegram.org
-  "API_HASH": os.getenv("API_HASH", ""),
+  "API_HASH": os.getenv("API_HASH", "7699560031"),
   # Get From my.telegram.org
-  "API_ID": os.getenv("API_ID", ""),
+  "API_ID": os.getenv("API_ID", "a184b91d39fc85265e232e7c323fac45"),
   #Get For @BotFather
   "BOT_TOKEN": os.getenv("BOT_TOKEN", ""),
   # Get For tembo.io
-  "DATABASE_URL_PRIMARY": "postgresql://neondb_owner:npg_NoDFHanE76qR@ep-twilight-fire-a80mx792-pooler.eastus2.azure.neon.tech/neondb?sslmode=require",
+  "DATABASE_URL_PRIMARY": "postgresql://neondb_owner:npg_UdVNDuv71gHm@ep-icy-violet-a8q6p6us-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require",
   # Logs Channel Username Without @
-  "CACHE_CHANNEL": "Dump2075",
+  "CACHE_CHANNEL": "",
   # Force Subs Channel username without @
   "CHANNEL": "",
   # {chap_num}: Chapter Number
   # {chap_name} : Manga Name
   # Ex : Chapter {chap_num} {chap_name} @Manhwa_Arena
-  "FNAME": "[MC] [{chap_num}] {chap_name} @Manga_Campus"  
+  "FNAME": "[CH - {chap_num}] {chap_name} @Manga_Unity"  
 
 }
 dbname = env_vars.get('DATABASE_URL_PRIMARY') or env_vars.get('DATABASE_URL') or 'sqlite:///test.db'
